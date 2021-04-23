a0700485ecf665d1eef914ce8e4201fac8a00994
Joao Henrique Vasconcelos <limajoaohenrique1@gmail.com>
2021-04-23 10:57:17 -0300

    changing the app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import os
 
 from app.misc import get_unique_diameters_list
-from database_handler.Build_JSON_db import build_json_db(), does_local_db_exist
 
 import streamlit as st
 
@@ -14,13 +13,11 @@ infelizmente o formato em que os dados deles é disponibilizado é uma :poop:
 
 Esse App pretende resolver isso :smiley:
 '''
-
-if not does_local_db_exist(os.getcwd()+"/data/"):
-  build_json_db()
   
 unique_diameters_list = get_unique_diameters_list()
 
 diameter = st.select_slider("Slide to select the propeller's diameter", options=unique_diameters_list)
 
-pitches_list = get_pitches()
-pitch = st.select_slider("Slide to select the propeller's diameter", options=pitches_list)
+unique_pitches_list = get_unique_pitches(unique_diameters_list)
+
+pitch = st.select_slider("Slide to select the propeller's diameter", options=unique_pitches_list)
