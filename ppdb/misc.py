a4a4329a4a176bb4a0c7ba7a0168ed347d78c3d8
Joao Henrique Vasconcelos <limajoaohenrique1@gmail.com>
2021-04-23 10:58:08 -0300

    minor changes

--- ppdb/misc.py
+++ ppdb/misc.py
@@ -1,11 +1,17 @@
 import re
+import os
 
 def get_model_name(file_path):
     with open(file_path, "r") as file:
-        file_lines = file.readlines(1)
+        model_line = file.readline()
     file.close()
 
-    line = file_lines[0]
-    line = line.strip()
-    line = re.sub(" +", "", line)
-    return line.split("(")[0]
+    model_name = model_line
+    model_name = model_name.strip()
+    model_name = re.sub(" +", "", model_name)
+    return model_name.split("(")[0]
+
+
+if __name__ == "__main__":
+    test_file = os.getcwd()+"/data/raw_files/PER3_4x4E-3.dat"
+    print("Model name: ", get_model_name(test_file))
